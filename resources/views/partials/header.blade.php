2ca42bcabc7921e84c0f95e7c41cf977dee528eb
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2021-01-07 16:57:05 +0100

    Stilato header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,83 @@
+<header>
+    <div class="navbar-top">
+        <div class="container">
+                <a href="#">
+                    <p class="text-uppercase">dc power&#8480;  visa&#174; </p>
+                </a>
+                <a href="#">
+                    <p class="text-uppercase">additional dc sites</p>
+                </a>
+        </div>
+    </div>
+
+    <div class="navbar-bottom">
+        <div class="container">
+            <nav>
+                <div class="logo">
+                    <img src="{{ asset('images/dc-logo.png')}}" alt="">
+                </div>
+
+                <div class="nav-menu">
+                    <ul>
+                        <li>
+                            <a href="#" class="text-uppercase">
+                                characters
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-uppercase active">
+                                comics
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-uppercase">
+                                movies
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-uppercase">
+                                tv
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-uppercase">
+                                games
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-uppercase">
+                                collectibles
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-uppercase">
+                                videos
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-uppercase">
+                                fans
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-uppercase">
+                                news
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-uppercase">
+                                shop
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="navbar-search">
+                    <input id="search" type="search" name="" value="search" placeholder="Search">
+                </div>
+            </nav>
+        </div>
+    </div>
+
+
+</header>
